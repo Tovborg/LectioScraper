9a6009a2d672996d841d4c04f48f12f93bd95105
Tovborg <emiltovborg11@gmail.com>
2022-09-13 14:58:00 +0200

    added manifest.in

--- setup.py
+++ setup.py
@@ -21,9 +21,10 @@ setup(
     url="https://github.com/Tovborg/LectioScraper",
     download_url="https://pypi.org/project/lectioscraper/",
     install_requires=['beautifulsoup4', 'requests', 'lxml', 'pytz'],
-    # make sure credentials.json is included in the package
-    package_dir={'lectioscraper': 'lectioscraper'},
-    data_files=[('lectioscraper', ['lectioscraper/credentials.json'])],
+    # credentials.json is located in the lectioscraper directory with all the other python files for the package. It is not included in the package. include it
+    include_package_data=True,
+
+    
     
     keywords=['python', 'Lectio', 'Scraping', 'webscraping'],
 )
