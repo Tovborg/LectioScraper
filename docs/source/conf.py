713a1045f5b2ae0c05a10cab51b7ecf64d2c2d3a
Emil Tovborg-Jensen <emiltovborg-jensen@Emils-MacBook-Pro.local>
2023-11-07 16:29:13 +0100

    changed conf.py for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ release = '3.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx_autodoc_typehints']
+extensions = ['sphinx.ext.autodoc', 'sphinx-autodoc-typehints']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
